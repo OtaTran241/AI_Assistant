9ffc3f43a69d3f87db5245e43914900ec7157088
OtaTran <otatran241@gmail.com>
2025-01-03 22:34:28 +0700

    Update_RAG_Commit

--- src/Agentic_Rag/Create_vdb.py
+++ src/Agentic_Rag/Create_vdb.py
@@ -1,15 +1,28 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader, JSONLoader
-from langchain.document_loaders import Docx2txtLoader
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader, JSONLoader, Docx2txtLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+import torch
+import faiss
 
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+# ✅ Sử dụng GPU nếu có
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+# embedding_model = HuggingFaceEmbeddings(
+#     model_name="vinai/phobert-large",
+#     model_kwargs={"device": device}
+# )
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="dangvantuan/vietnamese-embedding",
+    model_kwargs={"device": device}
+)
 
 def create_db_from_files(data_folder):
+    pypdf_loader_kwargs = {'extract_images': True}
     loaders = [
         DirectoryLoader(data_folder, glob="*.txt", loader_cls=TextLoader),
-        DirectoryLoader(data_folder, glob="*.pdf", loader_cls=PyPDFLoader),
+        DirectoryLoader(data_folder, glob="*.pdf", loader_cls=PyPDFLoader, loader_kwargs=pypdf_loader_kwargs),
         DirectoryLoader(data_folder, glob="*.docx", loader_cls=Docx2txtLoader),
         DirectoryLoader(data_folder, glob="*.json", loader_cls=JSONLoader)
     ]
@@ -17,10 +30,16 @@ def create_db_from_files(data_folder):
     documents = []
     for loader in loaders:
         documents.extend(loader.load())
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=150)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=100)
     docs = text_splitter.split_documents(documents)
 
     db = FAISS.from_documents(documents=docs, embedding=embedding_model)
+
+    if device == "cuda":
+        print("Chuyển FAISS index sang GPU...")
+        res = faiss.StandardGpuResources()
+        db.index = faiss.index_cpu_to_gpu(res, 0, db.index)
+
     db.save_local(f"{data_folder}/Vector_db")
     return db
 
